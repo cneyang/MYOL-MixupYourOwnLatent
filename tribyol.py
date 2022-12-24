7f1decb847e4f14eecd4c05974ca7130faadd9d9
cneyang <cneyang@g.skku.edu>
2022-12-24 11:35:16 +0900

    original tribyol

--- tribyol.py
+++ tribyol.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', default='cifar10', type=str, help='Dataset')
     parser.add_argument('--batch_size', default=100, type=int, help='Number of images in each mini-batch')
-    parser.add_argument('--epochs', default=80, type=int, help='Number of sweeps over the dataset to train')
+    parser.add_argument('--epochs', default=100, type=int, help='Number of sweeps over the dataset to train')
     parser.add_argument('--optim', default='sgd', type=str, help='Optimizer')
     parser.add_argument('--lr', default=0.03, type=float, help='Learning rate')
     parser.add_argument('--seed', default=0, type=int, help='Random seed')
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 
     batch_size, epochs = args.batch_size, args.epochs
     
-    model_name = 'tribyol_original_{}'.format(args.optim, args.seed)
+    model_name = 'tribyol_original_{}'.format(args.seed)
 
     result_path = f'{args.dataset}/results_tribyol_batch{batch_size}/'
     if not os.path.exists(result_path):
--- utils.py
+++ utils.py
@@ -7,35 +7,6 @@ from torchvision.datasets import CIFAR10, Flowers102, FGVCAircraft
 from torch.utils.data.sampler import SubsetRandomSampler
 
 
-def create_datasets(batch_size, train_data):
-    # trainning set 중 validation 데이터로 사용할 비율
-    valid_size = 0.2
-
-    # validation으로 사용할 trainning indices를 얻는다.
-    num_train = len(train_data)
-    indices = list(range(num_train))
-    np.random.shuffle(indices)
-    split = int(np.floor(valid_size * num_train))
-    train_idx, valid_idx = indices[split:], indices[:split]
-
-    # trainning, validation batch를 얻기 위한 sampler정의
-    train_sampler = SubsetRandomSampler(train_idx)
-    valid_sampler = SubsetRandomSampler(valid_idx)
-
-    # load training data in batches
-    train_loader = torch.utils.data.DataLoader(train_data,
-                                               batch_size=batch_size,
-                                               sampler=train_sampler,
-                                               num_workers=4, drop_last=True)
-
-    # load validation data in batches
-    valid_loader = torch.utils.data.DataLoader(train_data,
-                                               batch_size=128,
-                                               sampler=valid_sampler,
-                                               num_workers=4, drop_last=True)
-
-    return train_loader, valid_loader
-
 class GaussianBlur(object):
 
     def __init__(self, sigma=[.1, 2.]):
@@ -79,64 +50,6 @@ class CIFAR10Triplet(CIFAR10):
 
         return pos_1, pos_2, pos_3, target
 
-class Flowers102Pair(Flowers102):
-    def __getitem__(self, index):
-        image_file, target = self._image_files[index], self._labels[index]
-        img = Image.open(image_file).convert('RGB')
-
-        if self.transform is not None:
-            pos_1 = self.transform(img)
-            pos_2 = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return pos_1, pos_2, target
-
-class Flowers102Triplet(Flowers102):
-    def __getitem__(self, index):
-        image_file, target = self._image_files[index], self._labels[index]
-        img = Image.open(image_file).convert('RGB')
-
-        if self.transform is not None:
-            pos_1 = self.transform(img)
-            pos_2 = self.transform(img)
-            pos_3 = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return pos_1, pos_2, pos_3, target
-
-class AircraftPair(FGVCAircraft):
-    def __getitem__(self, idx):
-        image_file, label = self._image_files[idx], self._labels[idx]
-        img = Image.open(image_file).convert('RGB')
-
-        if self.transform is not None:
-            pos_1 = self.transform(img)
-            pos_2 = self.transform(img)
-
-        if self.target_transform is not None:
-            label = self.target_transform(label)
-
-        return pos_1, pos_2, label
-
-class AircraftTriplet(FGVCAircraft):
-    def __getitem__(self, idx):
-        image_file, label = self._image_files[idx], self._labels[idx]
-        img = Image.open(image_file).convert('RGB')
-
-        if self.transform is not None:
-            pos_1 = self.transform(img)
-            pos_2 = self.transform(img)
-            pos_3 = self.transform(img)
-
-        if self.target_transform is not None:
-            label = self.target_transform(label)
-
-        return pos_1, pos_2, pos_3, label
-
 train_transform = transforms.Compose([
     transforms.RandomResizedCrop(32),
     transforms.RandomHorizontalFlip(p=0.5),
@@ -151,38 +64,6 @@ tribyol_transform = transforms.Compose([
     transforms.RandomApply([transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)], p=0.8),
     transforms.RandomGrayscale(p=0.2),
     transforms.GaussianBlur(kernel_size=9),
-    transforms.ToTensor(),
-    transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
-
-flowers_transform = transforms.Compose([
-    transforms.RandomResizedCrop(224, scale=(0.2, 1.0)),
-    transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
-    transforms.RandomGrayscale(p=0.2),
-    transforms.RandomApply([GaussianBlur([.1, 2.])], p=0.5),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-
-flowers_test_transform = transforms.Compose([
-    transforms.Resize(224),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-
-strong_transform = transforms.Compose([
-    transforms.RandomResizedCrop(96, scale=(0.2, 1.0)),
-    transforms.RandomHorizontalFlip(p=0.75),
-    transforms.RandomApply([transforms.ColorJitter(1.6, 1.6, 1.6, 0.4)], p=0.8),
-    transforms.RandomGrayscale(p=0.4),
-    transforms.GaussianBlur(kernel_size=13),
-    transforms.ToTensor(),
-    transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
-
-weak_transform = transforms.Compose([
-    transforms.RandomResizedCrop(96, scale=(0.2, 1.0)),
-    transforms.RandomHorizontalFlip(p=0.25),
-    transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
-    transforms.RandomGrayscale(p=0.1),
-    transforms.GaussianBlur(kernel_size=5),
     transforms.ToTensor(),
     transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
 
