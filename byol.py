fde4ec4c0d1e1327ed4ae34a702a23df49a60d35
cneyang <cneyang@g.skku.edu>
2023-03-17 15:52:14 +0900

    rename variable

--- byol.py
+++ byol.py
@@ -75,10 +75,10 @@ def update_moving_average(ema_updater, ma_model, current_model):
 
 # MLP class for projector and predictor
 class MLP(nn.Module):
-    def __init__(self, dim, projection_size, hidden_size = 4096):
+    def __init__(self, input_dim, projection_size, hidden_size = 4096):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(dim, hidden_size),
+            nn.Linear(input_dim, hidden_size),
             nn.BatchNorm1d(hidden_size),
             nn.ReLU(inplace=True),
             nn.Linear(hidden_size, projection_size)
