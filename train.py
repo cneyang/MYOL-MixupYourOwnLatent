a474f7c35ebff1beb3d09d05d38d82c7380e953c
cneyang <cneyang@g.skku.edu>
2023-04-28 00:57:42 +0900

    dev init

---
+++ train.py
@@ -0,0 +1,105 @@
+import os
+import argparse
+import pandas as pd
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+import numpy as np
+
+import utils
+from model import Model
+from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', default='cifar10', type=str, help='Dataset')
+    parser.add_argument('--batch_size', default=128, type=int, help='Number of images in each mini-batch')
+    parser.add_argument('--epochs', default=100, type=int, help='Number of sweeps over the dataset to train')
+    parser.add_argument('--algo', default='byol', type=str, help='Algorithm')
+    parser.add_argument('--alpha', default=1.0, type=float, help='mixup alpha')
+    parser.add_argument('--seed', default=0, type=int, help='Random seed')
+    args = parser.parse_args()
+
+    model_name = f'{args.algo}_{args.seed}'
+
+    result_path = f'dev/{args.dataset}/results_{args.algo}_batch{args.batch_size}/'
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+
+    print(model_name)
+    if os.path.exists(result_path+f'{model_name}_{args.epochs}.pth'):
+        print(model_name, 'already exists')
+        import sys
+        sys.exit()
+        
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    
+    writer = SummaryWriter('runs/' + f'{args.dataset}/batch{args.batch_size}/' + model_name)
+
+    if args.dataset == 'cifar10':
+        train_transform = utils.CIFAR10Pair.get_transform(train=True)
+        train_data = utils.CIFAR10Pair(root='./data', train=True, transform=train_transform, download=True)
+        train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    elif args.dataset == 'cifar100':
+        train_transform = utils.CIFAR100Pair.get_transform(train=True)
+        train_data = utils.CIFAR100Pair(root='./data', train=True, transform=train_transform, download=True)
+        train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=4, pin_memory=True)
+
+    results = {'train_loss': []}
+    
+    model = Model().cuda()
+
+    # get class
+    algo = getattr(__import__('algo'), args.algo.upper())
+
+    learner = algo(
+        model.f,
+        image_size=32,
+        hidden_layer=-2,
+        projection_size=128,
+        projection_hidden_size=512,
+    )
+
+    optimizer = optim.Adam(learner.parameters(), lr=5e-4, weight_decay=1e-6)
+    scaler = torch.cuda.amp.GradScaler()
+    least_loss = np.Inf
+    
+    for epoch in range(1, args.epochs + 1):
+        # train
+        total_loss, total_num = 0, 0
+        data_bar = tqdm(train_loader)
+
+        learner.train()
+        for x1, x2, _ in data_bar:
+            batch_size = x1.size(0)
+            x1, x2 = x1.cuda(), x2.cuda()
+            
+            with torch.cuda.amp.autocast():
+                loss = learner(x1, x2)
+                print(loss.item())
+
+            total_num += batch_size
+            total_loss += loss.item() * batch_size
+
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            learner.update_moving_average()
+
+            data_bar.set_description('Epoch: [{}/{}] Train Loss: {:.4f}'.format(epoch, args.epochs, total_loss / total_num))
+        
+        train_loss = total_loss / total_num
+        writer.add_scalar('train_loss', train_loss, epoch)
+        results['train_loss'].append(train_loss)
+        
+        # save statistics
+        data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
+        data_frame.to_csv(result_path+f'{model_name}_statistics.csv', index_label='epoch')
+        if epoch % 10 == 0:
+            torch.save(model.state_dict(), result_path+f'{model_name}_{epoch}.pth')
